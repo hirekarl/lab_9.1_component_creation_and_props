b53eba7347c1697b7a307fea95f6ac64951cec10
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-07-23 19:49:21 -0400

    Added destructured props for ProductDisplay and UserProfileCard.

---
+++ component-library/src/components/ProductDisplay/ProductDisplay.tsx
@@ -0,0 +1,11 @@
+import { type ProductDisplayProps } from "../../types"
+
+export default function ProductDisplay({
+  product,
+  showDescription,
+  showStockStatus,
+  onAddToCart,
+  children,
+}: ProductDisplayProps) {
+  return <div>ProductDisplay</div>
+}
---
+++ component-library/src/components/UserProfileCard/UserProfileCard.tsx
@@ -0,0 +1,11 @@
+import { type UserProfileCardProps } from "../../types"
+
+export default function UserProfileCard({
+  user,
+  showEmail,
+  showRole,
+  onEdit,
+  children,
+}: UserProfileCardProps) {
+  return <div></div>
+}
