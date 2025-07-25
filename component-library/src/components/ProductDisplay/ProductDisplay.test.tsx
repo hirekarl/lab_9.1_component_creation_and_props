acedb16f3e4d9d0b1cb694b3f3ca61f027772ca6
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-07-25 11:07:53 -0400

    Renamed randomProduct -> fidgetSpinner, added return of void to handleAddToCart.

--- component-library/src/components/ProductDisplay/ProductDisplay.test.tsx
+++ component-library/src/components/ProductDisplay/ProductDisplay.test.tsx
@@ -2,7 +2,7 @@ import { type Product } from "../../types"
 import ProductDisplay from "./ProductDisplay"
 
 export default function ProductDisplayTest() {
-  const randomProduct: Product = {
+  const fidgetSpinner: Product = {
     id: "27d1661f-65cb-453a-ae9a-88cb93df151f",
     name: "Fidget Spinner",
     price: 17.99,
@@ -13,13 +13,13 @@ export default function ProductDisplayTest() {
       "https://upload.wikimedia.org/wikipedia/commons/f/f3/Fidget_spinner_red%2C_cropped.jpg",
   }
 
-  function handleAddToCart(id: string) {
+  function handleAddToCart(id: string): void {
     alert(`You clicked the "Add to Cart" button for product with ID ${id}.`)
   }
 
   return (
     <ProductDisplay
-      product={randomProduct}
+      product={fidgetSpinner}
       showDescription={true}
       showStockStatus={true}
       onAddToCart={handleAddToCart}>
