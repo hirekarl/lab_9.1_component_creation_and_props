bb417879344df05dfbe3f1839fecccf5d0ea8f91
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-07-25 07:28:07 -0400

    Added Sections to App.tsx.

--- component-library/src/App.tsx
+++ component-library/src/App.tsx
@@ -1,11 +1,19 @@
+import Section from "./components/Section/Section"
 import AlertBoxTest from "./components/AlertBox/AlertBox.test"
 import UserProfileCardTest from "./components/UserProfileCard/UserProfileCard.test"
 
 function App() {
   return (
     <>
-      <AlertBoxTest />
-      <UserProfileCardTest />
+      <h1 className="my-3">Lab 9.1: Component Creation and Props</h1>
+      <hr />
+      <Section title="Alert Box">
+        <AlertBoxTest />
+      </Section>
+      <hr />
+      <Section title="User Profile Card">
+        <UserProfileCardTest />
+      </Section>
     </>
   )
 }
