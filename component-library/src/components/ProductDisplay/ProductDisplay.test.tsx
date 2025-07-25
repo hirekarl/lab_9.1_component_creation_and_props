2dc0d1e95f0c02ca2e4a3d4a91cb71472509d844
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-07-25 10:32:52 -0400

    Finnessed ProductDisplayTest.

--- component-library/src/components/ProductDisplay/ProductDisplay.test.tsx
+++ component-library/src/components/ProductDisplay/ProductDisplay.test.tsx
@@ -6,14 +6,15 @@ export default function ProductDisplayTest() {
     id: "27d1661f-65cb-453a-ae9a-88cb93df151f",
     name: "Fidget Spinner",
     price: 17.99,
-    description: "A normal, non-haunted fidget spinner. Your children can safely play with it without summoning the Dark Lord.",
+    description:
+      "A normal, non-haunted fidget spinner. Your children can safely play with it—the Dark Lord won't appear in your home even once.",
     inStock: true,
     imageUrl:
       "https://upload.wikimedia.org/wikipedia/commons/f/f3/Fidget_spinner_red%2C_cropped.jpg",
   }
 
   function handleAddToCart(id: string) {
-    alert(`Add to Cart button clicked for product with ID ${id}`)
+    alert(`"Add to Cart" button clicked for product with ID ${id}.`)
   }
 
   return (
@@ -22,7 +23,10 @@ export default function ProductDisplayTest() {
       showDescription={true}
       showStockStatus={true}
       onAddToCart={handleAddToCart}>
-      <p className="text-warning"><i className ="bi bi-exclamation-triangle-fill"></i> Hurry! These are going fast!</p>
+      <p className="text-warning">
+        <i className="bi bi-exclamation-triangle-fill"></i> Hurry! These are
+        going fast!
+      </p>
     </ProductDisplay>
   )
 }
