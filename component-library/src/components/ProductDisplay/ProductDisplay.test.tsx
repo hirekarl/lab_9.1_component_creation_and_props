f66b06b29b4b6e21b766858bcf3916486ba65641
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-07-25 08:55:11 -0400

    Implemented ProductDisplay except for callback function portion.

---
+++ component-library/src/components/ProductDisplay/ProductDisplay.test.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { type Product } from "../../types"
+import ProductDisplay from "./ProductDisplay"
+
+export default function ProductDisplayTest() {
+  const randomProduct: Product = {
+    id: "27d1661f-65cb-453a-ae9a-88cb93df151f",
+    name: "Fidget Spinner",
+    price: 17.99,
+    description: "A normal, non-haunted fidget spinner. Your children can safely play with it without summoning the Dark Lord.",
+    inStock: true,
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/f/f3/Fidget_spinner_red%2C_cropped.jpg",
+  }
+
+  return (
+    <ProductDisplay
+      product={randomProduct}
+      showDescription={true}
+      showStockStatus={true}>
+      <p className="text-warning"><i className ="bi bi-exclamation-triangle-fill"></i> Hurry! These are going fast!</p>
+    </ProductDisplay>
+  )
+}
--- component-library/src/components/ProductDisplay/ProductDisplay.tsx
+++ component-library/src/components/ProductDisplay/ProductDisplay.tsx
@@ -1,5 +1,4 @@
 import { type ProductDisplayProps } from "../../types"
-
 export default function ProductDisplay({
   product,
   showDescription,
@@ -7,5 +6,39 @@ export default function ProductDisplay({
   onAddToCart,
   children,
 }: ProductDisplayProps) {
-  return <div>ProductDisplay</div>
+  const { id, name, price, description, inStock, imageUrl } = product
+
+  return (
+    <div className="row">
+      <div className="col-xl-4 offset-xl-4 col-lg-4 offset-lg-4 col-md-6 offset-md-3 col-sm-12">
+        <div className="card text-center">
+          <img
+            className="card-img-top"
+            src={imageUrl}
+            alt={`Preview image of ${name}.`}
+          />
+          <div className="card-body">
+            <h3 className="card-title fs-5">{name}</h3>
+            <p className="text-primary fw-bold">
+              {price.toLocaleString("en-US", {
+                style: "currency",
+                currency: "USD",
+              })}
+            </p>
+            {showDescription && <p>{description}</p>}
+            {showStockStatus &&
+              (inStock ? (
+                <p className="text-success">In Stock</p>
+              ) : (
+                <p className="text-danger">Out of Stock</p>
+              ))}
+              {children}
+            <button type="button" className="btn btn-primary w-100">
+              Add to Cart
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
